44b55412ce19ea65357e34da2d63c05300c5c5ce
Ansel Orhero <86934203+Kkshanana90@users.noreply.github.com>
2021-11-08 16:08:12 +0100

    Add files via upload

---
+++ Cleaning_Nairaland_Scraping.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+full_data= pd.read_csv('Nairaland_df.csv',lineterminator='\n')
+full_data.head(20)
+
+
+# In[ ]:
+
+
+
+def examine(comments):
+    if comment== np.nan or comments == None or len(comments.split()) < 2:
+        return False
+    else:
+        return True
+
+
+# In[ ]:
+
+
+mask = df.loc[:, 'Comments'].map(examine)
+df_clean = df[mask]
+df_clean
+
